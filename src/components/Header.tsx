9d2a63f2decf510bfe0a789e88d742a72ead115b
GiovanniRS <giovannirodrigues.2012@gmail.com>
2024-11-25 20:41:35 -0300

    Add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 export default function Header(){
     return(
         <header className="container mx-auto py-4 flex justify-between">
+            <h1 className="text-4xl font-bold bg-gradient-to-r from-violet-gr to-pink-gr bg-clip-text text-fill-transparent">&lt;GIO&frasl;&gt;</h1>
             
         </header>
     )

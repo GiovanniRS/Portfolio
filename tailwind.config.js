14f28eee857964c13c9c3ef486c4402ac3f8358c
GiovanniRS <giovannirodrigues.2012@gmail.com>
2024-11-25 19:25:55 -0300

    Adicionando fontfamily do site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jetBrains: ["JetBrains Mono", "monospace"]
+      }
+    },
   },
   plugins: [],
 }

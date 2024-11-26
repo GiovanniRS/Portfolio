3fca09bbe358fd3dd7ebce8fe214a695d7a7b9db
Giovanni R Santos <giovannirodrigues.2012@gmail.com>
2024-11-26 16:39:05 -0300

    Adicionando animacao

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,16 @@
-import { PiLinkedinLogo, PiGithubLogo, PiInstagramLogo } from "react-icons/pi";
+import { LuLinkedin, LuGithub, LuInstagram, LuChevronDown } from "react-icons/lu";
 
 export default function Hero() {
     return(
         <section className="h-screen container mx-auto flex flex-col justify-center">
             <p className="text-4xl font-bold leading-snug">Olá! Eu sou o <br />Giovanni Rodrigues, <br />Desenvolvedor <span className="bg-gradient-to-r from-violet-gr to-pink-gr bg-clip-text text-fill-transparent">FullStack</span> <br />e <span className="bg-gradient-to-r from-violet-gr to-pink-gr bg-clip-text text-fill-transparent">Professor</span></p>
-            <div className="flex gap-5">
-                <a href="https://br.linkedin.com/in/giovannirodrigues" target="_blank"><PiLinkedinLogo className="text-4xl" /></a>
-                <a href="https://github.com/giovannirs" target="_blank"><PiGithubLogo className="text-4xl" /></a>
-                <a href="" target="_blank"><PiInstagramLogo className="text-4xl" /></a>
+            <div className="flex gap-5 mt-4">
+                <a href="https://br.linkedin.com/in/giovannirodrigues" target="_blank"><LuLinkedin className="text-3xl" /></a>
+                <a href="https://github.com/giovannirs" target="_blank"><LuGithub className="text-3xl" /></a>
+                <a href="https://www.instagram.com/gir0drigues/" target="_blank"><LuInstagram className="text-3xl" /></a>
+            </div>
+            <div className="flex justify-center">
+                <LuChevronDown className="text-3xl absolute bottom-10 arrow-scroll" />
             </div>
         </section>
     )

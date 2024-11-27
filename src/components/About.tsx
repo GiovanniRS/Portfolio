7f5bdffda960a8d8c8a74ccc0646ec974bf5fa54
Giovanni R Santos <giovannirodrigues.2012@gmail.com>
2024-11-27 17:21:02 -0300

    Adicionando foto na section sobre

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,16 @@
+import foto from "../assets/foto.png"
+import hexagono from "../assets/hexagono.svg"
+
 export default function About() {
     return(
         <section className="min-h-screen py-14 bg-slate-50 dark:bg-neutral-900">
             <div className="container mx-auto">
                 <h2 className="text-4xl font-bold text-center">Sobre</h2>
                 <div className="h-1 w-2/3 mx-auto mt-8 bg-gradient-to-r from-violet-gr to-pink-gr rounded-full " />
+                <figure className="w-1/3 flex justify-center items-end relative">
+                    <img src={foto} alt="" className="w-10/12" />
+                    <img src={hexagono} alt="" className="absolute top-0 w-11/12" />
+                </figure>
             </div>
         </section>
     )
